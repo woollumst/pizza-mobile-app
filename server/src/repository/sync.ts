585dc565016193ffd8adb637106c91fdbe03cedc
Toby Woollums <woollumst@gmail.com>
2025-03-30 13:38:13 -0700

    updates

--- server/src/repository/sync.ts
+++ server/src/repository/sync.ts
@@ -1,11 +1,15 @@
 import sequelize from './sequelize';
 // import models here
 // import User from '../models/User';
+import MenuItem from '../models/menuItem';
 
 const syncDatabase = async () => {
     try {
         await sequelize.sync({ force: true });
+        console.log('Database synced successfully');
     } catch (error) {
         console.error('Error syncing database:', error);
     }
 }
+
+syncDatabase();
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+// import authRoutes from "./controllers/authController.js"; //sample for importing routes (1/2)
 
 dotenv.config();
 const app = express();
@@ -9,16 +10,45 @@ const host = process.env.HOST || 'localhost';
 
 app.get('/', async (req, res) => { // Test endpoint to ensure server is live
     res.send('Hello World!');
-    // try{ // old PG setup
-    //     const result = await client.query("SELECT NOW()");
-    //     res.send(`Database connected! Server time: ${result.rows[0].now}`);
-    // } catch(error) {
-    //     res.status(500).send('Database connection failed');
-    // }
 });
+
+// app.use("/auth", authRoutes); // sample for importing routes (2/2)
 
 app.listen(port, () => { // Launch server
     console.log(`Server listening at http://${host}:${port}`);
 });
 
 export { app }; // exporting app for testing
+
+/*
+old server.js from taskmanagement app : 
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import client from "./repository/db.js";
+import authRoutes from "./controllers/authController.js";
+import taskRoutes from "./controllers/taskController.js";
+
+dotenv.config();                            //env variables
+const app = express();                      //load express
+const PORT = process.env.PORT || 5000;      //grab port variable from env if available
+app.use(express.json());                    // load json compatibility
+app.use(cors());                            // load cors obj
+
+app.use("/auth", authRoutes); // login, register routing and token dispersal handled in auth.js
+app.use("/tasks", taskRoutes);
+
+app.get('/', async (req, res) => { //home page
+    try{ // test database connection?
+        const result = await client.query('SELECT NOW()'); //test
+        res.send(`Database connected! Server time: ${result.rows[0].now}`);
+    } catch (error) {
+        res.status(500).send('Database connection failed');
+    }
+});
+
+// app.get('/api/protected', authenticate, (req, res) => { // Test endpoint to test JWT authentication
+//     res.json({ message: ' you have access to this protected route', user: req.user });
+// });
+
+*/
