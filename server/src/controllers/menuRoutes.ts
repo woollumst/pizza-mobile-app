5531d2fc2a751a5413ef9b4b3d4574eb46f659bb
Toby <woollumst@gmail.com>
2025-04-03 17:24:54 -0700

    Rough draft for menuRoutes

--- server/src/controllers/menuRoutes.ts
+++ server/src/controllers/menuRoutes.ts
@@ -1,18 +1,59 @@
 // Managing menuItems. Viewing menu, handling item info when requested etc
+import express from "express";
+import menuService from “../service/menuService”;
+// import authenticate from "../middleware/authMiddleware";
 
+const menuRoutes = express.Router();
 
-
-
+// /menu endpoints ( hostname/menu/:id )
 // Get /                Get full menu
+menuRoutes.get(‘/’, async (req, res) => {
+// May need to refactor for TS Sequelize
+try{
+        const result = await menuService.getMenu();
+        const menu = result.menu;
+        if(!result.success){
+            res.status(404).json({ error: 'Failed to fetch menu items' });
+        }
+        res.status(200).json({
+            success: true,
+            menu: menu.map(menuItem => ({
+                itemId: menuItem.itemId,
+                itemName: menuItem.itemName,
+                description: menuItem.description,
+                price: menuItem.price,
+                category: menuItem.category,
+                createdAt: menuItem.createdAt,
+                updatedAt: menuItem.updatedAt,
+            }))
+        }); // Need to test
+    } catch (error) {
+        console.log('Server error with fetching menu');
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Error getting menu', error: 'Failed to get menu items' });
+    }
+*/
+});
 
 // Get /:id             Get one item from menu
+menuRoutes.get(‘/:id’, async (req, res) => {
 
+});
 
-// ADMIN Commands for modifying menu. 
-// Implement Role Based Access Control so only admin accounts can modify menu
-
+// Admin Commands (Implement Role-Based Access Control)
 // Post /:id            Add new item (ADMIN)
+menuRoutes.post(‘/:id’, async (req, res) => {
+
+});
 
 // Put /:id             Update item (ADMIN)
+menuRoutes.put(‘/:id’, async (req, res) => {
 
-// Delete /:id          Delete item (ADMIN) (add a confirmation dialogue and maybe an undo button later)
+});
+
+// Delete /:id          Delete item (ADMIN) (add confirmation dialogue, maybe undo button)
+menuRoutes.delete(‘/:id’, async (req, res) => {
+
+});
+
+export default menuRoutes;
