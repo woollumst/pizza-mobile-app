9df3993e677858224a1ee7947d7cd08985d8dd63
Toby Woollums <woollumst@gmail.com>
2025-04-06 14:44:59 -0700

    MenuItem Updates

--- server/src/repository/menuRepository.ts
+++ server/src/repository/menuRepository.ts
@@ -2,24 +2,59 @@ import MenuItem from "../models/MenuItem";
 
 export class menuRepository {
     // Get all Menu Items
-    async getMenu(){
-        return await MenuItem.findAll();
+    static async getMenu(){
+        try{
+            const fullMenu = await MenuItem.findAll();  // Return all Menu Items (Full Menu)
+            return fullMenu;
+        } catch (error) {
+            console.error('Database error, failed to get Menu');
+            throw new Error('Database getMenu query failed');
+        }
     }
     // Get one Menu Item by Id
-    async getMenuItem(itemId: number){
-        return await MenuItem.findByPk(itemId);
+    static async getMenuItem(itemId: number){
+        try{
+            const menuItem = await MenuItem.findByPk(itemId); // Return one menu item by Primary Key (ID)
+            return menuItem;
+        } catch (error) {
+            console.error('Database error, failed to get Menu Item');
+            throw new Error('Database getMenuItem query failed');
+        }
     }
     // Admin Commands
     // Create Menu Item
-    async createMenuItem(newMenuItem: MenuItem){
-        await newMenuItem.save();
+    static async createMenuItem(newMenuItem: MenuItem){
+        try{
+            await newMenuItem.save(); // Save new menu item
+            return await newMenuItem.reload(); // Refresh with persisted data, return new item from database
+        } catch (error) {
+            console.error('Database error, failed to create Menu Item');
+            throw new Error('Database createMenuItem query failed');
+        }
     }
     // Update a Menu Item by Id
-    async updateMenuItem(updatedItem: MenuItem){
-        await updatedMenuItem.save();
+    static async updateMenuItem(updatedItem: MenuItem){
+        try{
+            await updatedItem.save();
+            return await updatedItem.reload();
+        } catch (error) {
+            console.error('Database error, failed to update Menu Item');
+            throw new Error('Database updateMenuItem query failed');
+        }
     }
     // Delete a Menu Item by Id
-    async deleteMenuItem(itemToDelete: MenuItem){
-        await itemToDelete.destroy();
+    static async deleteMenuItem(itemId: number){
+        try{
+            const itemToDelete = await this.getMenuItem(itemId);
+            if (itemToDelete) {
+                await itemToDelete.destroy();
+                return true;
+            }
+            console.error("itemToDelete cannot be null!");
+            return false;
+        } catch (error) {
+            console.error('Database error, failed to delete Menu Item');
+            throw new Error('Database deleteMenuItem query failed');
+        }
     }
 }
--- server/src/service/menuService.ts
+++ server/src/service/menuService.ts
@@ -2,7 +2,7 @@ import MenuItem from "../models/MenuItem";
 import { menuRepository } from "../repository/menuRepository";
 
 export class menuService {
-    constructor(menuRepository: menuRepository) {}, // prep dependency injection for mocks/testing
+    constructor(private menuRepository: menuRepository) {} // prep dependency injection for mocks/testing
 
     async getMenu() { // TEST
         try{
@@ -14,13 +14,16 @@ export class menuService {
             }
         } catch (error) {
             console.error('getMenu error: ', error);
-            return (success: false, message: 'Internal server error'); 
+            return {
+                success: false, 
+                message: 'Internal server error'
+            }
         }
-    },
+    }
 
-    async getMenuItem() { // TEST
+    async getMenuItem(itemId: number) { // TEST
         try{
-            const result = await menuRepository.getById();
+            const result = await menuRepository.getMenuItem(itemId);
             return {
                 success: true,
                 message: 'Menu item fetched successfully',
@@ -28,43 +31,67 @@ export class menuService {
             }
         } catch (error) {
             console.error('getMenuItem error: ', error);
-            return (success: false, message: 'Internal server error'); 
+            return {
+                success: false, 
+                message: 'Internal server error'
+            }
         }
-    },
+    }
 
     async createMenuItem(newItem: MenuItem) { // TODO
         try { // old ? review, refactor
-            const newMenuItem = MenuItem.build({ 
-                itemName: newItem.itemName, 
-                // description: newItem.description || null, 
-                price: newItem.price, 
-                category: newItem.category,
-                createdAt: newItem.createdAt,
-                updatedAt: newItem.updatedAt,
-            });
-            return menuRepository.createMenuItem(newMenuItem);
+            const result = await menuRepository.createMenuItem(newItem);
+            return {
+                success: true,
+                message: 'Menu item created successfully',
+                menuItem: result,
+            }
         } catch (error) {
-            
+            console.error('createMenuItem error: ', error);
+            return {
+                success: false, 
+                message: 'Internal server error'
+            }
         }
-    },
+    }
 
-    async updateMenuItem() { // TODO
+    async updateMenuItem(updatedItem: MenuItem) { // TODO
         try{
-            
+            const result = await menuRepository.updateMenuItem(updatedItem);
+            return {
+                success: true,
+                message: 'Menu item updated successfully',
+                menuItem: result,
             }
         } catch (error) {
             console.error('updateMenuItem error: ', error);
-            return (success: false, message: 'Internal server error'); 
+            return {
+                success: false, 
+                message: 'Internal server error'
+            }
         }
-    },
+    }
 
-    async deleteMenuItem() { // TODO
+    async deleteMenuItem(itemId: number) { // TODO
         try{
-            
+            const result = await menuRepository.deleteMenuItem(itemId);
+            if (result) {
+                return {
+                    success: true,
+                    message: 'Menu item deleted successfully',
+                }
+            } else {
+                return {
+                    success: false,
+                    message: 'Item to delete is null!',
+                }
             }
         } catch (error) {
             console.error('deleteMenuItem error: ', error);
-            return (success: false, message: 'Internal server error'); 
+            return {
+                success: false, 
+                message: 'Internal server error'
+            }
         }
     }
 }
