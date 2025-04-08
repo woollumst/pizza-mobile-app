adef76153a8c1405283d7d496bc1d71fce3d3662
Toby <woollumst@gmail.com>
2025-04-07 20:00:24 -0700

    Update db.ts for testing

--- server/src/repositories/db.ts
+++ server/src/repositories/db.ts
@@ -5,13 +5,25 @@ dotenv.config({
     path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 }); // For tests, use test env, so separate db can be used
 
-const sequelize = new Sequelize({
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
+const commonConfig = {
+  dialect: process.env.DB_DIALECT as any,
+  // models: [__dirname + '/../models'], // alternate ORM method? review, refactor?
+  // logging: process.env.NODE_ENV !== 'test' // Logging when not in test env
+};
+
+const sequelize =
+  process.env.DB_DIALECT === 'sqlite'
+    ? new Sequelize({
+        ...commonConfig,
+        storage: process.env.DB_STORAGE || ':memory:'
+      })
+    : new Sequelize({
+        ...commonConfig,
+        host: process.env.DB_HOST,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+      });
 
 const testConnection = async () => {
     try{
