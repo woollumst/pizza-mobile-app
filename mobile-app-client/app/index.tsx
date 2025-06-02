458f14c3e9a17d760c7902368469d644b882edcb
Toby Woollums <woollumst@gmail.com>
2025-06-01 20:58:25 -0700

    Converted to Expo, tested router between screens

---
+++ mobile-app-client/app/index.tsx
@@ -0,0 +1,18 @@
+import { Link } from "expo-router";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Home Screen</Text>
+      <Link href="/cart">Go to Cart</Link>
+    </SafeAreaView>
+  );
+}
